f8306c6183c44a6bb936a99dd8b7d1eda6f42d18
GlennClaesPXL <148762236+GlennClaesPXL@users.noreply.github.com>
2025-10-08 21:14:58 +0200

    Register and Login endpoint

---
+++ EasyInvoice/src/main/java/be/glennclaes/easyinvoice/service/ProfileServiceImpl.java
@@ -0,0 +1,56 @@
+package be.glennclaes.easyinvoice.service;
+
+import be.glennclaes.easyinvoice.entity.UserEntity;
+import be.glennclaes.easyinvoice.io.ProfileRequest;
+import be.glennclaes.easyinvoice.io.ProfileResponse;
+import be.glennclaes.easyinvoice.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class ProfileServiceImpl implements ProfileService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public ProfileResponse createProfile(ProfileRequest request) {
+        UserEntity newProfile = convertToUserEntity(request);
+        if(!userRepository.existsByEmail(request.getEmail())){
+            newProfile = userRepository.save(newProfile);
+            return convertToProfileResponse(newProfile);
+        }
+        throw new ResponseStatusException(HttpStatus.CONFLICT, "Email already exists");
+    }
+
+    private ProfileResponse convertToProfileResponse(UserEntity newProfile) {
+        return ProfileResponse.builder()
+                .name(newProfile.getName())
+                .email(newProfile.getEmail())
+                .userId(newProfile.getUserId())
+                .isAccountVerified(newProfile.getIsAccountVerified())
+                .build();
+    }
+
+    private UserEntity convertToUserEntity(ProfileRequest request) {
+        return UserEntity.builder()
+                .email(request.getEmail())
+                .userId(UUID.randomUUID().toString())
+                .name(request.getName())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .isAccountVerified(false)
+                .resetOtpExpireAt(0L)
+                .verifyOtp(null)
+                .verifyOtpExpireAt(0L)
+                .resetOtp(null)
+                .build();
+
+    }
+
+}
