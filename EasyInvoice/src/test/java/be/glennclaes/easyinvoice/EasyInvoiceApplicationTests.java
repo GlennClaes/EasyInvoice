a8b4c6ff170f46161506d70efdf03d4fe8dc55a4
GlennClaesPXL <148762236+GlennClaesPXL@users.noreply.github.com>
2025-10-08 17:34:35 +0200

    Base project

---
+++ EasyInvoice/src/test/java/be/glennclaes/easyinvoice/EasyInvoiceApplicationTests.java
@@ -0,0 +1,13 @@
+package be.glennclaes.easyinvoice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyInvoiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
