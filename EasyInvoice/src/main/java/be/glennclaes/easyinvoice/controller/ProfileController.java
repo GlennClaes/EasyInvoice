995ece03b7d625fba825b1de8c52c5d3167ebab9
GlennClaesPXL <148762236+GlennClaesPXL@users.noreply.github.com>
2025-10-09 10:11:25 +0200

    Mail service

--- EasyInvoice/src/main/java/be/glennclaes/easyinvoice/controller/ProfileController.java
+++ EasyInvoice/src/main/java/be/glennclaes/easyinvoice/controller/ProfileController.java
@@ -2,6 +2,7 @@ package be.glennclaes.easyinvoice.controller;
 
 import be.glennclaes.easyinvoice.io.ProfileRequest;
 import be.glennclaes.easyinvoice.io.ProfileResponse;
+import be.glennclaes.easyinvoice.service.EmailService;
 import be.glennclaes.easyinvoice.service.ProfileService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -14,12 +15,13 @@ import org.springframework.web.bind.annotation.*;
 public class ProfileController {
 
     private final ProfileService profileService;
+    private final EmailService emailService;
 
     @PostMapping("/register")
     @ResponseStatus(HttpStatus.CREATED)
     public ProfileResponse register(@Valid @RequestBody ProfileRequest request){
         ProfileResponse response = profileService.createProfile(request);
-        //TODO: send welcome email
+        emailService.sendWelcomeEmail(response.getEmail(), response.getName());
         return response;
     }
     @GetMapping("/profile")
