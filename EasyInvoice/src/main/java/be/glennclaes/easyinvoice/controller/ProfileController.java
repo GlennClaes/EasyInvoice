35e553a993363aba7b0078b5b08a8ac336ed3ae4
GlennClaesPXL <148762236+GlennClaesPXL@users.noreply.github.com>
2025-10-09 09:49:42 +0200

    Profile Endpoint

--- EasyInvoice/src/main/java/be/glennclaes/easyinvoice/controller/ProfileController.java
+++ EasyInvoice/src/main/java/be/glennclaes/easyinvoice/controller/ProfileController.java
@@ -6,6 +6,7 @@ import be.glennclaes.easyinvoice.service.ProfileService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.CurrentSecurityContext;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -21,7 +22,8 @@ public class ProfileController {
         //TODO: send welcome email
         return response;
     }
-
-    
-
+    @GetMapping("/profile")
+    public ProfileResponse getProfile(@CurrentSecurityContext(expression =  "authentication?.name") String email){
+        return profileService.getProfile(email);
+    }
 }
